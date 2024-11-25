a40d6a54baa8794848426511e1d1d974eca2aab3
NataliaSh012 <shnatalya1989@gmail.com>
2024-11-25 14:05:21 +0200

    add sorting by table fields

--- src/components/AddCompanyTable/AddCompanyTable.tsx
+++ src/components/AddCompanyTable/AddCompanyTable.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import classes from './AddCompanyTable.module.scss';
-import { useReactTable, getCoreRowModel  } from "@tanstack/react-table";
-import type {
-  AddCompanyTableComponentProps,
-  TableRow,
-} from "./AddCompanyTable.type";
-
+import React, { useState } from "react";
+import classes from "./AddCompanyTable.module.scss";
+import {
+  useReactTable,
+  getCoreRowModel,
+  getSortedRowModel,
+} from "@tanstack/react-table";
+import type { AddCompanyTableComponentProps } from "./AddCompanyTable.type";
+import { Vacancy } from "../VacancyModal/VacancyModal.type";
+import SortIcon from "/src/assets/sort.svg";
 
 export const AddCompanyTable: React.FC<AddCompanyTableComponentProps> = ({
   onEdit,
   onDelete,
   data,
 }) => {
+  const [sorting, setSorting] = useState([]);
   const columns = [
     { accessorKey: "company", header: "Компания" },
     { accessorKey: "position", header: "Вакансия" },
@@ -21,7 +24,8 @@ export const AddCompanyTable: React.FC<AddCompanyTableComponentProps> = ({
     {
       id: "actions",
       header: "Действия",
-      cell: ({ row }: { row: { original: TableRow } }) => (
+      enableSorting: false,
+      cell: ({ row }: { row: { original: Vacancy } }) => (
         <div>
           <button onClick={() => onEdit(row.original)}>Edit</button>
           <button onClick={() => onDelete(row.original._id)}>Delete</button>
@@ -33,21 +37,59 @@ export const AddCompanyTable: React.FC<AddCompanyTableComponentProps> = ({
   const table = useReactTable({
     data,
     columns,
+    state: {
+      sorting,
+    },
+    onSortingChange: setSorting,
     getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
   });
 
+  if (data.length === 0) {
+    return (
+      <p className={classes.noDataMessage}>
+        Еще никто не добавил вакансию. Будьте первым!
+      </p>
+    );
+  }
   return (
     <table className={classes.table}>
       <thead>
         {table.getHeaderGroups().map((headerGroup) => (
           <tr key={headerGroup.id}>
             {headerGroup.headers.map((header) => (
-              <th key={header.id}>
+              <th
+                key={header.id}
+                onClick={
+                  header.column.getCanSort()
+                    ? header.column.getToggleSortingHandler()
+                    : undefined
+                }
+                style={{
+                  cursor: header.column.getCanSort() ? "pointer" : "default",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
                 {header.isPlaceholder
                   ? null
                   : header.column.columnDef.header instanceof Function
                   ? header.column.columnDef.header(header.getContext())
                   : header.column.columnDef.header}
+                {header.column.getCanSort() && (
+                  <img
+                    src={SortIcon}
+                    alt="Sort Icon"
+                    style={{
+                      marginLeft: "8px",
+                      width: "16px",
+                      height: "16px",
+                      filter: header.column.getIsSorted()
+                        ? "brightness(0)" // Темнее, если сортировка включена
+                        : "brightness(0.6)", // Светлее, если сортировки нет
+                    }}
+                  />
+                )}
               </th>
             ))}
           </tr>
